62e5087852ec138e216f6350b765e88cc5739279
John M <johnmaiconnp@hotmail.com>
2022-05-17 10:45:32 -0300

    add data for cards

--- src/app/service/poke-api.service.ts
+++ src/app/service/poke-api.service.ts
@@ -6,7 +6,7 @@ import { map, Observable, tap } from 'rxjs';
   providedIn: 'root',
 })
 export class PokeApiService {
-  private url: string = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=100';
+  private url: string = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20';
 
   constructor(private http: HttpClient) {}
 
