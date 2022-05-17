0f4b5c1e670734562899a5db4175bcc64bf8a4cb
John M <johnmaiconnp@hotmail.com>
2022-05-17 14:17:41 -0300

    add filter search

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -7,14 +7,25 @@ import { PokeApiService } from 'src/app/service/poke-api.service';
   styleUrls: ['./poke-list.component.scss'],
 })
 export class PokeListComponent implements OnInit {
-  getAllPokemons: any;
+
+  public getAllPokemons: any;
+  private setAllPokemons: any;
 
   constructor(private pokeApiService: PokeApiService) {}
 
   ngOnInit(): void {
     this.pokeApiService.apiListAllPokemons.subscribe((res) => {
       this.getAllPokemons = res.results;
-      console.log(this.getAllPokemons);
+      this.setAllPokemons = this.setAllPokemons;
     });
+  }
+
+  getSearch(value: string) {
+    console.log(value);
+    const filter = this.getAllPokemons.filter(
+      (res: any) => !res.name.indexOf(value.toLowerCase())
+    );
+
+    this.getAllPokemons = filter;
   }
 }
